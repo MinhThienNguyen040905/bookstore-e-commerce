2476c6aba533d6835171ca9675e09a4512376ee0
NguyenMinhThien <nguyenminhthienph1449@gmail.com>
2025-09-27 20:51:25 +0700

    feat: gift assignment

--- frontend/frontend-app/src/App.js
+++ frontend/frontend-app/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const gifts = ['candy', 'toys', 'soft drinks']
 
 function App() {
+
+  const [gift, setGift] = useState()
+  const randomGift = () => {
+    const index = Math.floor(Math.random() * gifts.length)
+    console.log(gifts[index]);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: 30 }}>
+      <h1>Hello World</h1>
+      <button onClick={randomGift} >Random Gift</button>
     </div>
   );
 }
